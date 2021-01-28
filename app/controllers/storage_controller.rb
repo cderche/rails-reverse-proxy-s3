b82283f2530e502c4ff8f2235455534f81cc9d02
Cyrille <c.derche@me.com>
2021-01-28 11:37:25 +0100

    presigned_url

--- app/controllers/storage_controller.rb
+++ app/controllers/storage_controller.rb
@@ -3,9 +3,19 @@ class StorageController < ApplicationController
     data = S3_BUCKET.presigned_post(
       key: "uploads/#{SecureRandom.uuid}/${filename}",
       success_action_status: "201",
-      acl: "public-read"
+      acl: "public-read",
     )
 
     render json: { url: data.url, fields: data.fields }, status: :ok
+  end
+
+  def presigned_url
+    filepath = params[:filepath]
+    data = S3_BUCKET.object(filepath).presigned_url(
+      :get,
+      expires_in: 3600,
+    )
+    puts data
+    render json: { url: data }, status: :ok
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   get 'documents/upload'
   get 'storage/presigned_post'
+  get 'storage/presigned_url'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
