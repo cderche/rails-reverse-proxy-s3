f9e7fc84479a107c9ff2025edb181084e4e828ab
Cyrille <c.derche@me.com>
2021-01-28 10:21:10 +0100

    presigned_post

--- app/controllers/storage_controller.rb
+++ app/controllers/storage_controller.rb
@@ -1,9 +1,9 @@
 class StorageController < ApplicationController
-  def upload
+  def presigned_post
     data = S3_BUCKET.presigned_post(
       key: "uploads/#{SecureRandom.uuid}/${filename}",
-      success_action_status: 201,
-      acl: 'public-read'
+      success_action_status: "201",
+      acl: "public-read"
     )
 
     render json: { url: data.url, fields: data.fields }, status: :ok
